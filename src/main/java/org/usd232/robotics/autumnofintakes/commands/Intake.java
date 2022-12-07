9f9e523f76cd8064472810f28971457c73c29a83
Noah Johnson <johnsnoa001@usd232.org>
2022-12-06 22:23:53 -0600

    added IntakeSubsystem and Intake Command

--- src/main/java/org/usd232/robotics/autumnofintakes/commands/Intake.java
+++ src/main/java/org/usd232/robotics/autumnofintakes/commands/Intake.java
@@ -1,7 +1,38 @@
 package org.usd232.robotics.autumnofintakes.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
+import org.usd232.robotics.autumnofintakes.log.Logger;
+import org.usd232.robotics.autumnofintakes.subsystems.IntakeSubsystem;
 
-public class Intake {
+public class Intake extends CommandBase {
+    /**
+     * The logger.
+     *
+     * @since 2018
+     */
+    @SuppressWarnings("unused")
+    private static final Logger LOG = new Logger();
     
+    private final IntakeSubsystem intakeSubsystem;
+
+    private double speed;
+
+    public Intake (double speed, IntakeSubsystem intakeSubsystem) {
+        this.speed = speed;
+
+        this.intakeSubsystem = intakeSubsystem;
+
+        addRequirements(intakeSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        intakeSubsystem.intake(speed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intakeSubsystem.intake(0.0);
+    }
 }
--- src/main/java/org/usd232/robotics/autumnofintakes/subsystems/IntakeSubsystem.java
+++ src/main/java/org/usd232/robotics/autumnofintakes/subsystems/IntakeSubsystem.java
@@ -1,7 +1,28 @@
 package org.usd232.robotics.autumnofintakes.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import org.usd232.robotics.autumnofintakes.log.Logger;
+
+import org.usd232.robotics.autumnofintakes.Constants.IntakeConstants;
+
 public class IntakeSubsystem extends SubsystemBase {
-    
+    /**
+     * The logger.
+     *
+     * @since 2022
+     */
+    // @SuppressWarnings("unused")
+    private static final Logger LOG = new Logger();
+
+    private final Victor intakeMotor;
+
+    public IntakeSubsystem() {
+        intakeMotor = LOG.catchAll(() -> new Victor(IntakeConstants.INTAKE_MOTOR));
+    }
+
+    public void intake (double speed) {
+        intakeMotor.set(speed);
+    }
 }
